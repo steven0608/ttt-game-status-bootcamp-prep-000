a82b36d67cf7b613adcc7a6aedab5352fb3f2ac5
Steven Chen <ysteven.chen2@gmail.com>
2018-04-17 19:33:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6
 
 def won?(board)
   WIN_COMBINATIONS.each do |x|
-  if board[x[0]]==board[x[1]] && board[x[1]]==board[x[2]]
+  if board[x[0]]==board[x[1]] && board[x[1]]==board[x[2]] && board[x[1]]== "X" || board[x[0]]==board[x[1]] && board[x[1]]==board[x[2]] && board[x[1]]== "O"
     return x
   else
     false
